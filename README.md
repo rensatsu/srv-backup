cd41d12c58edb3b1ef73b7311df9b59c991808fa
Pavel Tvaladze <reddragonsportal@gmail.com>
2018-03-23 17:46:49 +0300

    Moved description from script source to readme

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
 # Server Backup
 
 Script for creating backups and uploading to Dropbox.
+
+## Task Files
+
+This script is using task files located in
+SCRIPT_DIR/tasks/*.txt
+
+Example of the task file:
+```
+@ENABLED=1
+@TASK=ExampleTask
+@PASSWORD=P4$$W0RD
+@PATHS
+/path/test/abc
+/path/test/def
+/etc/other/path
+```
+
+Description of fields:
+
+* `@ENABLED` = [0, 1]: 1 - task enabled, 0 - disabled.
+* `@TASK` = string: name of the task (only letters, numbers or underscores).
+* `@PASSWORD` = string: backup archive password.
+* `@PATHS` = section: after this line there should only be a list with paths to backup.
+
+## Decryption
+
+```
+gpg -d FILENAME.tar.gpg > FILENAME.tar
+```
--- backup.py
+++ backup.py
@@ -3,38 +3,6 @@
 # Script for creating backups and uploading to Dropbox     #
 # ##########################################################
 
-# ##########################################################
-# Task files                                               #
-# ##########################################################
-# This script is using task files located in               #
-# SCRIPT_DIR/tasks/*.txt                                   #
-#                                                          #
-# * Example of the task file:                              #
-#                                                          #
-# @ENABLED=1                                               #
-# @TASK=ExampleTask                                        #
-# @PASSWORD=P4$$W0RD                                       #
-# @PATHS                                                   #
-# /path/test/abc                                           #
-# /path/test/def                                           #
-# /etc/other/path                                          #
-#                                                          #
-# * Description of fields:                                 #
-#   @ENABLED = [0, 1]: 1 - task enabled, 0 - disabled      #
-#   @TASK = string: name of the task (only letters, num-   #
-#           bers or underscores                            #
-#   @PASSWORD = string: backup archive password            #
-#   @PATHS = section: after this line there should only    #
-#            be a list with paths to backup                #
-# ##########################################################
-
-# ##########################################################
-# Information                                              #
-# ##########################################################
-# Decryption example:                                      #
-# gpg -d saver_backup_1219.tar.gpg > saver_backup_1219.tar #
-# ##########################################################
-
 import os.path
 import re
 import sys
